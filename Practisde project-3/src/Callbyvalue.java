072a0624648c615c08e4321eb81afc282e8211dc
admin <admin@public1.114dns.com>
2022-07-25 21:50:33 +0530

    Overloading

---
+++ Practisde project-3/src/Callbyvalue.java
@@ -0,0 +1,16 @@
+public class Callbyvalue {
+
+int val=150;
+
+int operation(int val) {
+	val =val*10/100;
+	return(val);
+}
+
+public static void main(String args[]) {
+	Callbyvalue d = new Callbyvalue();
+	System.out.println("Before operation value of data is "+d.val);
+	d.operation(100);
+	System.out.println("After operation value of data is "+d.val);
+	}
+}
