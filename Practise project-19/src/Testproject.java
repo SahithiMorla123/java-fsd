7d48e57fdd65200fe86d9d5688fc7dd5ebd42c06
admin <admin@public1.114dns.com>
2022-07-25 22:23:10 +0530

    project

---
+++ Practise project-19/src/Testproject.java
@@ -0,0 +1,28 @@
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class Testproject implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        Testproject ob = new Testproject(); 
+        ob.show(); 
+    } 
+}
